7e3061837ff7e76cbda840bd8b83cc2f0c34e24c
lenintarrillo <lenin.tarrillo.v@gmail.com>
2022-12-12 13:14:54 -0500

    data and png nft

--- ChocanoNFT.sol
+++ ChocanoNFT.sol
@@ -8,6 +8,10 @@
 //////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////CONTRATO INTELIGENTE PARA LIBRO TOKENIZADO//////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+///Project leader:  Eduardo Chocano
+///Project Advisor and Developer: Lenin Tarrillo
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
@@ -16,18 +20,13 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract ChocanoNFT is 
-    ERC721Enumerable, 
-    AccessControl, 
-    Pausable 
-{
+contract CoffeeEveryMorningNFT is ERC721Enumerable, AccessControl, Pausable {
     bytes32 public constant ADVISOR_ROLE = keccak256("ADVISOR_ROLE");
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     bool public saleActive = false;
     bool public aproveWithdrawFunds = false;
     bool public aproveReturnfunds = false;
-    
 
     // Reserved for the team, customs, giveaways, collabs and so on.
     uint256 public reserved = 15;
@@ -43,11 +42,13 @@ contract ChocanoNFT is
     uint256 public constant MAX_MINT_PER_TX = 5;
 
     // The base link that leads to the image / video of the token
-    string public baseTokenURI = "https://api.funkycrocs.io/";
+    string public baseTokenURI = "";
+
+    string public _WhitePaperURI = "";
 
     address public chocanoNFTAddress;
 
-    constructor(address _chocano) ERC721("ChocanoNFT", "CNFT") {
+    constructor(address _chocano) ERC721("The Coffee Every Morning NFT", "COF") {
         price = initial_price;
         _setupRole(ADVISOR_ROLE, _msgSender());
         chocanoNFTAddress = _chocano;
@@ -58,7 +59,6 @@ contract ChocanoNFT is
     function _baseURI() internal view virtual override returns (string memory) {
         return baseTokenURI;
     }
-
 
     // See which address owns which tokens
     function tokensOfOwner(address addr)
@@ -72,29 +72,24 @@ contract ChocanoNFT is
             tokensId[i] = tokenOfOwnerByIndex(addr, i);
         }
         return tokensId;
-        
     }
-
 
     // Standard mint function
-    function returnFunds(address holder) public  whenNotPaused {
-        require(aproveReturnfunds, "The Advisor has not activated the return of funds");
-        
+    function returnFunds(address holder) public whenNotPaused {
+        require(
+            aproveReturnfunds,
+            "The Advisor has not activated the return of funds"
+        );
+
         uint256 tokenCount = balanceOf(holder);
         uint256 tokenID = 0;
-       
+
         for (uint256 i; i < tokenCount; i++) {
             tokenID = tokenOfOwnerByIndex(holder, i);
-           _beforeTokenTransfer(holder, address(0),tokenID,0 );
-           
+            _beforeTokenTransfer(holder, address(0), tokenID, 0);
         }
-        payable(holder).transfer(price*tokenCount);
-
-
+        payable(holder).transfer(price * tokenCount);
     }
-
-
-
 
     // Standard mint function
     function mintToken(uint256 _amount) public payable whenNotPaused {
@@ -130,39 +125,41 @@ contract ChocanoNFT is
         saleActive = val;
     }
 
-     // Start and stop sale
+    // Activate the withdrawal of collected funds
     function setAproveWithdrawFunds() public onlyRole(ADVISOR_ROLE) {
-        aproveWithdrawFunds=true;
+        aproveWithdrawFunds = true;
     }
 
+    // Activate the return of funds
     function setAproveReturnfunds() public onlyRole(ADVISOR_ROLE) {
-        aproveReturnfunds=true;
+        aproveReturnfunds = true;
     }
-
-
 
     // Set new baseURI
     function setBaseURI(string memory baseURI) public onlyRole(ADVISOR_ROLE) {
         baseTokenURI = baseURI;
     }
 
-    // Set a different price in case ETH changes drastically
+    // Set WhitePaper Uri
+    function setWhitePaperUri(string memory uri) public onlyRole(ADVISOR_ROLE) {
+        _WhitePaperURI = uri;
+    }
+
     function setPrice(uint256 newPrice) public onlyRole(ADMIN_ROLE) {
         price = newPrice;
     }
 
-    function setChocanoAddress(address _chocano)
-        public
-        onlyRole(ADVISOR_ROLE)
-    {
+    function setChocanoAddress(address _chocano) public onlyRole(ADVISOR_ROLE) {
         chocanoNFTAddress = _chocano;
         _setupRole(ADMIN_ROLE, _chocano);
     }
 
     function withdrawFunds(uint256 amount) public payable onlyRole(ADMIN_ROLE) {
-        require(aproveWithdrawFunds,"The Advisor has not authorized the withdrawal of the fund");
+        require(
+            aproveWithdrawFunds,
+            "The Advisor has not authorized the withdrawal of the fund"
+        );
         require(payable(chocanoNFTAddress).send(amount));
-
     }
 
     function pause() public onlyRole(ADVISOR_ROLE) {
@@ -173,48 +170,33 @@ contract ChocanoNFT is
         _unpause();
     }
 
-
-
-    function whitepaperUri() public pure virtual returns (string memory) {
-        return  "";
-
-        }
-
-    
-    function chocano() public pure virtual returns (string memory) {
-        return "Eduardo Jesus Chocano Ravida"
-               "Estudiante de Derecho de la Universidad de Lima"
-               "https://www.linkedin.com/in/eduardo-jesus-chocano-ravina-9611541b6/";
-               
+    function whitepaperUri() public view returns (string memory) {
+        return _WhitePaperURI;
     }
 
+    function chocano() public pure virtual returns (string memory) {
+        return
+            "Eduardo Jesus Chocano Ravida"
+            "https://www.linkedin.com/in/eduardo-jesus-chocano-ravina-9611541b6/";
+    }
 
     function advisor() public pure virtual returns (string memory) {
-        return "Lenin Tarrillo Vilchez"
-               "Speaker en Blockchain y criptoactivos. Vicepresidente de la Asociacion de Blockchain del Peru"
-               "https://www.linkedin.com/in/lenintv/";
-               
+        return
+            "Lenin Tarrillo Vilchez"
+            "https://www.linkedin.com/in/lenintv/";
     }
-
-
 
     function version() public pure virtual returns (string memory) {
         return "1.0.0";
     }
 
-     
     // The following functions are overrides required by Solidity.
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(
-            ERC721Enumerable,
-            AccessControl
-        )
+        override(ERC721Enumerable, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
     }
-
-
 }
